93805227c9cc0883816bff6b6fba3c83d2e52c04
Abi Vickery <vickeryabi@gmail.com>
2019-09-30 13:35:21 -0500

    15
    answer

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ console.log(baz6);
 
 
 let baz8 = 5, baz9 = 10;
-let baz10 = baz8 + baz9;
+let baz10 = baz8 + baz9; // 15
 // let baz10 = 'baz8 + baz9'; // has already been declared once, can't be declared twice
 
 console.log(baz9);
